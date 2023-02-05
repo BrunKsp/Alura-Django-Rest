0b8e2d68cdcaa6bda6dd088339ac8549a97e6942
BrunoSiqueira <bbrunosiqueira14@gmail.com>
2023-02-05 18:46:18 -0300

    Adicionado validações e filtros

--- clientes/serializers.py
+++ clientes/serializers.py
@@ -8,14 +8,15 @@ class ClienteSerializer(serializers.ModelSerializer):
         fields = '__all__'
     def validate(self, data):
         if not cpf_valido(data['cpf']):
-            raise serializers.ValidationError({'cpf':"O CPF deve ter 11 dígitos"})
+            raise serializers.ValidationError({'cpf':"CPF inválido!"})
         if not nome_valido(data['nome']):
             raise serializers.ValidationError({'nome':"Não inclua números neste campo"})
         if not rg_valido(data['rg']):
             raise serializers.ValidationError({'rg':"O RG deve ter 9 dígitos"})
+        if not celular_valido(data['celular']):
+            raise serializers.ValidationError({'celular':"Deve seguir este modelo 11 22222-2222"})
+
+
         return data
 
-    # def validate_celular(self, celular):
-    #     if len(celular) < 11:
-    #         raise serializers.ValidationError("O celular deve ter 11 dígitos")
-    #     return celular
+    
